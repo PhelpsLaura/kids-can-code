80554cb129c46277f5e2ea87b13a62b29baab0e7
Laura Phelps <laphelps64@gmail.com>
2018-02-03 11:52:36 -0500

    whoa okay so the editor is up?

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :creations
+
+  root to: 'pages#show'
+
+  resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
